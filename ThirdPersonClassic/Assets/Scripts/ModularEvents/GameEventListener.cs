614dd7c58f1421ae4989f80178f0744b815d52e8
Oscar <oscarbuzzsmith@gmail.com>
2022-05-07 16:58:58 +1000

    Oscar Smith | 4 | Adding files back

---
+++ ThirdPersonClassic/Assets/Scripts/ModularEvents/GameEventListener.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameEventListener : MonoBehaviour
+{
+    [SerializeField] protected GameEvent _gameEvent;
+    [SerializeField] protected UnityEvent _unityEvent;
+
+    void Awake() => _gameEvent.Register(this);
+
+    void OnDestroy() => _gameEvent.Deregister(this);
+
+    public virtual void RaiseEvent() => _unityEvent.Invoke();
+}
